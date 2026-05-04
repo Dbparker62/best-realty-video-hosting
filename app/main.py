571d4c2da0025b0e6d2e458fafc7056626150ed5
Daniel Parker <danbparker@yahoo.com>
2026-05-03 22:20:28 -0400

    updating main for cors

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.routes.videos import router as videos_router
 from app.routes.auth import router as auth_router
 from app.routes.lessons import router as lessons_router
 from app.routes.admin import router as admin_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Real Estate Course Platform API")
 
 app.include_router(users_router)
@@ -16,6 +17,16 @@ app.include_router(auth_router)
 app.include_router(lessons_router)
 app.include_router(payments_router)
 app.include_router(admin_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://best-realty-video-hosting.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def health_check():
     return {"status": "ok"}
